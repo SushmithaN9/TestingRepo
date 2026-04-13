2a2c0035d56560b29763dbd3f4699dc9670f3c89
ST10457758 <st10457758@vcconnect.edu.za>
2026-04-13 17:59:01 +0200

    Files added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.10.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
 
     //  REQUIRED for room { }
