172de5874e9357498471fdf19abdb9ab9695ef0b
ST10457758 <st10457758@vcconnect.edu.za>
2026-04-13 18:57:04 +0200

    Added util and viewmodel

---
+++ app/src/main/java/com/example/testinggithub/util/SessionManager.kt
@@ -0,0 +1,22 @@
+package com.example.testinggithub.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SessionManager(context: Context) {
+
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("user_session", Context.MODE_PRIVATE)
+
+    fun saveUserId(userId: Int) {
+        prefs.edit().putInt("USER_ID", userId).apply()
+    }
+
+    fun getUserId(): Int {
+        return prefs.getInt("USER_ID", -1)
+    }
+
+    fun logout() {
+        prefs.edit().clear().apply()
+    }
+}
